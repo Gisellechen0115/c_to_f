1b4c8f0a1e005c1af84f5fafa0b1765d7fe3dc92
GiselleChen <50445469+Gisellechen0115@users.noreply.github.com>
2021-07-13 19:07:14 +0800

    Add files via upload
    using functions

--- c_to_f.py
+++ c_to_f.py
@@ -2,3 +2,12 @@ c = input('請輸入攝氏溫度:')
 c = float(c)
 f = c * 9/5 + 32
 print('華氏溫度:', f)
+
+
+celsius = int(input())
+
+def conv(celsius):
+    f =9/5*celsius +32
+    return f    
+fahrenheit = conv(celsius)
+print(fahrenheit)
